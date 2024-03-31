0599aecbf1544159b0b033474927b2a3f2a8c9e8
rose-salt <rachieru32@gmail.com>
2024-03-31 10:15:04 -0400

    Updated DML to reflect day change
    DML for classes had to be updated to reflect changes to ER, UML, and DDL

--- DML/DML_init_class.sql
+++ DML/DML_init_class.sql
@@ -4,14 +4,14 @@ INSERT INTO trainer (name, password, specialty) VALUES
     ('max muscle', 'gains4days', 'weight machines'),
     ('holly heartbeat', '95bpmandlovingit', 'zumba');
 
-INSERT INTO class(name, trainer_id, room_id, start, end) VALUES
-    ('CycleCraze Spin Studio', 2, 6, '2024-04-27 07:00:00', '2023-04-27 8:30:00'),
-    ('Zumba-Zest Fiesta', 4, 8, '2024-04-25 06:00:00', '2023-04-25 7:30:00'),
-    ('TreadTrek Treadmill Training', 2, 7, '2023-04-23 15:30:00', '2023-04-23 16:30:00');
+INSERT INTO class(name, trainer_id, room_id, day, start_time, end_time) VALUES
+    ('CycleCraze Spin Studio', 2, 6, 'SUN', '07:00:00', '08:30:00'),
+    ('Zumba-Zest Fiesta', 4, 8, 'MON','06:00:00', '07:30:00'),
+    ('TreadTrek Treadmill Training', 2, 7, 'TUE','15:30:00', '16:30:00');
 
-INSERT INTO personal_session(name, trainer_id, room_id, member_id, start, end) VALUES
-    ('LiftWell Personal Deadlift Training', 1, 2, 1, '2024-04-22 07:00:00', '2023-04-22 8:30:00'),
-    ('Machine Mastery', 3, 4, 2, '2024-04-21 06:00:00', '2023-04-21 7:30:00');
+INSERT INTO personal_session(name, trainer_id, room_id, member_id, day, start_time, end_time) VALUES
+    ('LiftWell Personal Deadlift Training', 1, 2, 1, 'WED','07:00:00', '08:30:00'),
+    ('Machine Mastery', 3, 4, 2, 'THU', '06:00:00', '07:30:00');
 
 INSERT INTO member_takes_class(member_id, class_id) VALUES
     (1, 1),
