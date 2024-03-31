6557977588f07127061907ee080ae4380f411269
Asianadian <danielchen.mc@gmail.com>
2024-03-31 10:29:25 -0400

    Fix member DML

--- DML/DML_init_members.sql
+++ DML/DML_init_members.sql
@@ -7,7 +7,7 @@ INSERT INTO member (name, password, age, weight, height, phone, address, email, 
 VALUES
   ('Isaac Roberts', '12345678', 30, 170, 170, '613-613-6133', '1 Malvern Street', 'isaac@gmail.com', 250, FALSE, 1),
   ('Rachel Steward', '12345678', 30, 140, 140, '613-613-6134', '2 Malvern Street', 'rachel@gmail.com', 250, FALSE, 1),
-  ('Dummy', '12345678', 30, 170, 170, '613-613-6133', '1 Malvern Street', 'dummy@gmail.com', 250, TRUE, 1);
+  ('Dummy', '12345678', 30, 170, 170, '0', '0 Malvern Street', 'dummy@gmail.com', 250, TRUE, 1);
 
 INSERT INTO goal (description, member_id)
 VALUES
