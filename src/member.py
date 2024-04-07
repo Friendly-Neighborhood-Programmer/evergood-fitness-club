d1d0bbe7c807ba40b9541fd213652588fc52bec5
Asianadian <danielchen.mc@gmail.com>
2024-04-07 15:07:28 -0400

    Added dashboard

--- src/member.py
+++ src/member.py
@@ -4,7 +4,7 @@ import sys
 # Connect to the A3 database
 def connect():
     con = psycopg2.connect(
-        dbname="test 7",
+        dbname="test 8",
         user="postgres",
         password="postgres",
         host="localhost",
@@ -82,7 +82,7 @@ def updatePersonalInformation(id, updateMap):
 
 #prints current member goals
 def viewGoals(id):
-    cursor.execute(f"SELECT * FROM goal WHERE goal.member_id = {id}")
+    cursor.execute(f"SELECT * FROM goal WHERE goal.member_id = {id} AND goal")
     res = cursor.fetchall()
 
     if (res):
@@ -119,12 +119,38 @@ def viewMetrics(id):
     else:
         print("Could not find metrics information.")
 
+#add achievement
+def addAchievement(id, name):
+    try:
+        cursor.execute(f"INSERT INTO achievement (name, member_id) \
+                        SELECT '{name}', {id}\
+                        WHERE NOT EXISTS (\
+                            SELECT *\
+                            FROM achievement\
+                            WHERE name = '{name}' AND member_id = {id}\
+                        );")
+        connection.commit()
+        return True
+    
+    except Exception as e:
+        print(str(e))
+        return False
+    
+def generateAchievement(id, name, val, min, step):
+    if val >= min + 2*step:
+        addAchievement(id, name + '++')
+    if val >= min + step:
+        addAchievement(id, name + '+')
+    if val >= min:
+        addAchievement(id, name)
+    
 #add weight entry, return True if successful, False otherwise
 def addWeight(id, kg):
     try:
         cursor.execute(f"INSERT INTO weight (kg, member_id) \
                        VALUES({kg}, {id});")
         connection.commit()
+        generateAchievement(id, 'Heavyweight', kg, 100, 50)
         return True
     
     except Exception as e:
@@ -137,6 +163,7 @@ def addSteps(id, count):
         cursor.execute(f"INSERT INTO step (count, member_id) \
                        VALUES({count}, {id});")
         connection.commit()
+        generateAchievement(id, 'Trekker', count, 20000, 5000)
         return True
     
     except Exception as e:
@@ -149,31 +176,130 @@ def addHeartrate(id, bpm):
         cursor.execute(f"INSERT INTO heartrate (bpm, member_id) \
                        VALUES({bpm}, {id});")
         connection.commit()
+        generateAchievement(id, "Pump", bpm, 100, 25)
         return True
     
     except Exception as e:
         print(str(e))
         return False
 
-viewPersonalInformation(9)
-update = {'name':'danny', 'age':2}
-updatePersonalInformation(9, update)
-viewPersonalInformation(9)
+def dashboardMenu():
+    print("(1) Routines\n(2) Achievements\n(3) Health Statistics(q) Back")
 
-viewGoals(9)
-createGoal(9, "weee")
-viewGoals(9)
+def routinesMenu():
+    print("(1) Change Routine")
 
-viewMetrics(9)
-addWeight(9, 12)
-addSteps(9, 1000)
-addHeartrate(9, 100)
-viewMetrics(9)
+def viewSelectedRoutine(id):
+    cursor.execute(f"SELECT routine.name, description\
+                    FROM member JOIN routine ON member.routine_id = routine.id\
+                    WHERE member.id = {id}")
+    res = cursor.fetchall()
+
+    if (res):
+        print(res)
+    else:
+        print("Could not find routine information.")
+
+def changeSelectedRoutine(id, routine_id):
+    try:
+        cursor.execute(f"UPDATE member\
+                        SET routine_id = {routine_id}\
+                        WHERE id = {id};")
+        connection.commit()
+        return True
+    
+    except Exception as e:
+        print(str(e))
+        return False
+
+def viewAchievements(id):
+    cursor.execute(f"SELECT name\
+                    FROM achievement\
+                    WHERE member_id = {id}\
+                    ORDER BY name ASC;")
+    res = cursor.fetchall()
+
+    if (res):
+        print(res)
+    else:
+        print("Could not find achievement information.")
+
+def viewWeightStatistics(id):
+    cursor.execute(f"SELECT MAX(kg) FROM weight GROUP BY member_id HAVING member_id = {id};")
+    max = cursor.fetchall()
+    cursor.execute(f"SELECT MIN(kg) FROM weight GROUP BY member_id HAVING member_id = {id};")
+    min = cursor.fetchall()
+    cursor.execute(f"SELECT AVG(kg) FROM weight GROUP BY member_id HAVING member_id = {id};")
+    avg = cursor.fetchall()
+
+    if (max or min or avg):
+        print(max) if max else None
+        print(min) if min else None
+        print(avg) if avg else None
+    else:
+        print("Could not find weight information.")
+
+def viewStepsStatistics(id):
+    cursor.execute(f"SELECT MAX(count) FROM step GROUP BY member_id HAVING member_id = {id};")
+    max = cursor.fetchall()
+    cursor.execute(f"SELECT MIN(count) FROM step GROUP BY member_id HAVING member_id = {id};")
+    min = cursor.fetchall()
+    cursor.execute(f"SELECT AVG(count) FROM step GROUP BY member_id HAVING member_id = {id};")
+    avg = cursor.fetchall()
+
+    if (max or min or avg):
+        print(max) if max else None
+        print(min) if min else None
+        print(avg) if avg else None
+    else:
+        print("Could not find steps information.")
+
+def viewHeartrateStatistics(id):
+    cursor.execute(f"SELECT MAX(bpm) FROM heartrate GROUP BY member_id HAVING member_id = {id};")
+    max = cursor.fetchall()
+    cursor.execute(f"SELECT MIN(bpm) FROM heartrate GROUP BY member_id HAVING member_id = {id};")
+    min = cursor.fetchall()
+    cursor.execute(f"SELECT AVG(bpm) FROM heartrate GROUP BY member_id HAVING member_id = {id};")
+    avg = cursor.fetchall()
+
+    if (max or min or avg):
+        print(max) if max else None
+        print(min) if min else None
+        print(avg) if avg else None
+    else:
+        print("Could not find heartrate information.")
+
+def viewHealthStatistics(id):
+    viewWeightStatistics(id)
+    viewStepsStatistics(id)
+    viewHeartrateStatistics(id)
+
+
+#viewPersonalInformation(9)
+#update = {'name':'danny', 'age':2}
+#updatePersonalInformation(9, update)
+#viewPersonalInformation(9)
+#
+#viewGoals(9)
+#createGoal(9, "weee")
+#viewGoals(9)
+#
+#viewMetrics(9)
+#addWeight(3, 200)
+#addSteps(3, 20000)
+#addHeartrate(3, 150)
+#viewMetrics(9)
 #print(login("isaac@gmail.com", "12345678"))
 #createNewMember("d", "12345678", 1, 1, 1, '1', '1', 'daniel@gmail.com')
 #print(login("daniel@gmail.com", "12345678"))
 
+#viewSelectedRoutine(3)
+##changeSelectedRoutine(3, 2)
+#viewSelectedRoutine(3)
 
+#viewAchievements(3)
+
+viewHealthStatistics(3)
 
 
 
