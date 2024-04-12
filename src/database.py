4e84fe47e22f7bf0c8a2fe97b8e1b1a2be9ca497
rose-salt <rachieru32@gmail.com>
2024-04-11 22:44:19 -0400

    make autocommit false
    :handbag: :boar:

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ import psycopg2
 class Database:
     def __init__(self, info):
         self.con = psycopg2.connect(database=info[0], user=info[1], password=info[2], host=info[3], port=info[4])
-        self.con.autocommit = True
+        self.con.autocommit = False
         self.cursor = self.con.cursor()
     
     def close(self):
