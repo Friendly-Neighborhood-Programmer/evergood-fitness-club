87c3fe41c888f91c5cf06f63b1b4aac471ae64a8
Asianadian <danielchen.mc@gmail.com>
2024-04-12 21:21:37 -0400

    fix unpaid and paid

--- src/admin.py
+++ src/admin.py
@@ -84,19 +84,19 @@ def member_pay_bill(connection, cursor, member_id):
         print(str(e))
         return False 
     
-def show_unpaid_members(connection, cursor):
+def show_unpaid_members(cursor):
     cursor.execute(f"SELECT member.id, member.name FROM member WHERE NOT member.paid;")
 
-    print(f"Unpaid Members\n{'id': ^4}|{'name': ^30}")
+    print(f"Unpaid Members\n{'id': ^4}|{'name': ^30}|{'cost': ^5}")
     for row in cursor.fetchall():
-        print(f"{row[0]: ^4}|{row[1]: ^30}")
+        print(f"{row[0]: ^4}|{row[1]: ^30}|{row[8]: ^5}")
 
-def show_paid_members(connection, cursor):
+def show_paid_members(cursor):
     cursor.execute(f"SELECT member.id, member.name FROM member WHERE member.paid;")
     
-    print(f"Paid Members\n{'id': ^4}|{'name': ^30}")
+    print(f"Paid Members\n{'id': ^4}|{'name': ^30}|{'cost': ^5}")
     for row in cursor.fetchall():
-        print(f"{row[0]: ^4}|{row[1]: ^30}")
+        print(f"{row[0]: ^4}|{row[1]: ^30}|{row[8]: ^5}")
 
 def get_all_classes(cursor):
     return trainer.get_all_classes(cursor)
